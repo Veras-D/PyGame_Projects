d23a2c976f50651bbaa17e66ae9b624efbe1919e
GabrielBandeiraDev <148816822+GabrielBandeiraDev@users.noreply.github.com>
2023-11-22 15:36:33 -0400

    Atualizar o main.py

--- main.py
+++ main.py
@@ -1,52 +1,22 @@
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.screenmanager import ScreenManager, Screen
+import pygame 
+import config
+from game_state import GameState
 
-class MainMenuScreen(Screen):
-    def _init_(self, **kwargs):
-        super(MainMenuScreen, self)._init_(**kwargs)
-        start_button = Button(text='Start Game', on_press=self.switch_to_game)
-        self.add_widget(start_button)
+from game import Game
 
-    def switch_to_game(self, instance):
-        self.manager.current = 'game'
+pygame.init()
 
-class GameScreen(Screen):
-    def _init_(self, **kwargs):
-        super(GameScreen, self)._init_(**kwargs)
-        
-        # Exemplo de botões para andar, bater e escolher
-        move_button = Button(text='Move', on_press=self.move_character)
-        attack_button = Button(text='Attack', on_press=self.attack_enemy)
-        choose_button = Button(text='Make a Choice', on_press=self.make_choice)
+screen = pygame.display.set_mode((600,400))
 
-        # Adicione os botões à interface do usuário
-        self.add_widget(move_button)
-        self.add_widget(attack_button)
-        self.add_widget(choose_button)
+pygame.display.set_caption("RPG Python")
 
-    def move_character(self, instance):
-        # Lógica para mover o personagem
-        print("Personagem se moveu.")
+clock = pygame.time.Clock()
 
-    def attack_enemy(self, instance):
-        # Lógica para atacar o inimigo
-        print("Personagem atacou o inimigo.")
 
-    def make_choice(self, instance):
-        # Lógica para fazer uma escolha
-        print("Jogador fez uma escolha.")
 
-# Gerenciador de telas
-class MyScreenManager(ScreenManager):
-    pass
+game = Game(screen)
 
-class RPGApp(App):
-    def build(self):
-        # Crie e retorne o gerenciador de tela
-        return MyScreenManager()
-
-# Execute o aplicativo
-if _name_ == '_main_':
-    RPGApp().run()
+while game.game_state==GameState.RUNNIG:  
+    clock.tick(50)
+    game.update()
+    pygame.display.flip()
